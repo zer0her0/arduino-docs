d442c3e627d4bcd086caa0d89604b54467f10a51
Andrew Meier <zer0her0@gmail.com>
2012-10-17 14:36:33 -0400

    working finally...
    though 0 in the tweet causes it not output count properly...

--- twitter2morse/twitter2morse.ino
+++ twitter2morse/twitter2morse.ino
@@ -5,8 +5,8 @@ My tweet2morse project.
 */
 #include <ctype.h>
 
-char tweet[140] = "@zer0her0 just a normal tweet";
-char *morseTweet[140][10];
+char tweet[140] = "normal tweet @";
+String morseTweet[140];
 int tweetLength = strlen(tweet);
 //int morseTweetLength = strlen(morseTweet);
 //int tweetLength = tweet.length();
@@ -26,262 +26,84 @@ void loop(){
 //blinkled();
 for (int i = 0; i < tweetLength; i++){
   tweet[i] = toupper(tweet[i]); 
-  Serial.println(tweet[i]); //print to console to make sure it's working
+  //Serial.println(tweet[i]); //print to console to make sure it's working
   
+  if (tweet[i] == 'A') { morseTweet[i] = ".-"; }
+  else if (tweet[i] == 'B') { morseTweet[i] = "-..."; }
+  else if (tweet[i] == 'C') { morseTweet[i] = "-.-."; }
+  else if (tweet[i] == 'D') { morseTweet[i] = "-.."; }
+  else if (tweet[i] == 'E') { morseTweet[i] = "."; }
+  else if (tweet[i] == 'F') { morseTweet[i] = "..-."; }
+  else if (tweet[i] == 'G') { morseTweet[i] = "--."; }
+  else if (tweet[i] == 'H') { morseTweet[i] = "...."; }
+  else if (tweet[i] == 'I') { morseTweet[i] = ".."; }
+  else if (tweet[i] == 'J') { morseTweet[i] = ".---"; }
+  else if (tweet[i] == 'K') { morseTweet[i] = "-.-"; }
+  else if (tweet[i] == 'L') { morseTweet[i] = ".-.."; }
+  else if (tweet[i] == 'M') { morseTweet[i] = "--"; }
+  else if (tweet[i] == 'N') { morseTweet[i] = "-."; }
+  else if (tweet[i] == 'O') { morseTweet[i] = "---"; }
+  else if (tweet[i] == 'P') { morseTweet[i] = ".--."; }
+  else if (tweet[i] == 'Q') { morseTweet[i] = "--.-"; }
+  else if (tweet[i] == 'R') { morseTweet[i] = ".-."; }
+  else if (tweet[i] == 'S') { morseTweet[i] = "..."; }
+  else if (tweet[i] == 'T') { morseTweet[i] = "-"; }
+  else if (tweet[i] == 'U') { morseTweet[i] = "..-"; }
+  else if (tweet[i] == 'V') { morseTweet[i] = "...-"; }
+  else if (tweet[i] == 'W') { morseTweet[i] = ".--"; }
+  else if (tweet[i] == 'X') { morseTweet[i] = "-..-"; }
+  else if (tweet[i] == 'Y') { morseTweet[i] = "-.--"; }
+  else if (tweet[i] == 'Z') { morseTweet[i] = "..--"; }
+  else if (tweet[i] == '0') { morseTweet[i] = "----"; }
+  else if (tweet[i] == '1') { morseTweet[i] = ".----"; }
+  else if (tweet[i] == '2') { morseTweet[i] = "..---"; }
+  else if (tweet[i] == '3') { morseTweet[i] = "...--"; }
+  else if (tweet[i] == '4') { morseTweet[i] = "....-"; }
+  else if (tweet[i] == '5') { morseTweet[i] = "....."; }
+  else if (tweet[i] == '6') { morseTweet[i] = "-...."; }
+  else if (tweet[i] == '7') { morseTweet[i] = "--..."; }
+  else if (tweet[i] == '8') { morseTweet[i] = "---.."; }
+  else if (tweet[i] == '9') { morseTweet[i] = "----."; }
+  else if (tweet[i] == '.') { morseTweet[i] = ".-.-.-"; }
+  else if (tweet[i] == ',') { morseTweet[i] = "--..--"; }
+  else if (tweet[i] == '?') { morseTweet[i] = "..--.."; }
+  else if (tweet[i] == '!') { morseTweet[i] = "-.-.--"; }
+  else if (tweet[i] == '@') { morseTweet[i] = ".--.-."; }
+  else if (tweet[i] == '$') { morseTweet[i] = "...-..-"; }
+  else if (tweet[i] == '&') { morseTweet[i] = ".-..."; }
+  else if (tweet[i] == '+') { morseTweet[i] = ".-.-."; }
+  else if (tweet[i] == '-') { morseTweet[i] = "-....-"; }
+  else if (tweet[i] == '_') { morseTweet[i] = "..--.-"; }
+  else if (tweet[i] == '=') { morseTweet[i] = "-...-"; }
+  else if (tweet[i] == ':') { morseTweet[i] = "---..."; }
+  else if (tweet[i] == ';') { morseTweet[i] = "-.-.-."; }
+  else if (tweet[i] == '\'') { morseTweet[i] = ".----."; }
+  else if (tweet[i] == '\"') { morseTweet[i] = ".-..-."; }
+  else if (tweet[i] == '/') { morseTweet[i] = "-..-."; }
+  else if (tweet[i] == '\\') { morseTweet[i] = "-..-."; }
+  else if (tweet[i] == '(') { morseTweet[i] = "-.--."; }
+  else if (tweet[i] == ')') { morseTweet[i] = "-.--.-"; }
+  /*else if (tweet[i] == '') {  }
+  else if (tweet[i] == '') {  }
+  else if (tweet[i] == '') {  }
+  else if (tweet[i] == '') {  }
+  else if (tweet[i] == '') {  }
+  else if (tweet[i] == '') {  }
+  else if (tweet[i] == '') {  }
+  else if (tweet[i] == '') {  }
+  else if (tweet[i] == '') {  }
+  else if (tweet[i] == '') {  }*/
+  else if (tweet[i] == ' ') { morseTweet[i] = " "; }
+  else {  }
   
+  Serial.println(morseTweet[i]);
   
-  /*for (int j = 0; j < 10; j++){
-    Serial.println(morseTweet[i][j]);
-  }*/
   delay(500);
   }
-//Serial.println("before convert");
-//morseconvert(morseTweet);
-//Serial.println("after convert");
+Serial.println(tweetLength);
 }
 
 //need morse code translation library/function here
 /*void blinkled() {
   
 }*/
-
-void morseconvert(char *morseTweet[140][10]){
-  Serial.println("before loop");
-  for (int i = 0; i < tweetLength; i++){
-    //convert to uppercase
-    //tweet[i] = toupper(tweet[i]);
-    //Serial.println(tweet[i]); //print to console to make sure it's working
-    //Serial.println("in loop");
-    //convert to morse
-    switch (tweet[i]) {
-      case 'A':
-        //do something when var equals A
-        morseTweet[i][10] = ".-";
-        break;
-      case 'B':
-        morseTweet[i][10] = "-...";
-        //do something when var equals B
-        break;
-      case 'C':
-        morseTweet[i][10] = "-.-.";
-        break;
-      case 'D':
-        morseTweet[i][10] = "-..";
-        break;
-      case 'E':
-        morseTweet[i][10] = ".";
-        break; 
-      case 'F':
-        morseTweet[i][10] = "..-.";
-        break; 
-      case 'G':
-        morseTweet[i][10] = "--.";
-        break; 
-      case 'H':
-        morseTweet[i][10] = "....";
-        break; 
-      case 'I':
-        morseTweet[i][10] = "..";
-        break; 
-      case 'J':
-        morseTweet[i][10] = ".---";
-        break; 
-      case 'K':
-        morseTweet[i][10] = "-.-";
-        break; 
-      case 'L':
-        morseTweet[i][10] = ".-..";
-        break; 
-      case 'M':
-        morseTweet[i][10] = "--";
-        break;
-      case 'N':
-        morseTweet[i][10] = "-.";
-        break; 
-      case 'O':
-        morseTweet[i][10] = "---";
-        break; 
-      case 'P':
-        morseTweet[i][10] = ".--.";
-        break; 
-      case 'Q':
-        morseTweet[i][10] = "--.-";
-        break; 
-      case 'R':
-        morseTweet[i][10] = ".-.";
-        break;
-      case 'S':
-        morseTweet[i][10] = "...";
-        break; 
-      case 'T':
-        morseTweet[i][10] = "-";
-        break; 
-      case 'U':
-        morseTweet[i][10] = "..-";
-        break; 
-      case 'V':
-        morseTweet[i][10] = "...-";
-        break; 
-      case 'W':
-        morseTweet[i][10] = ".--";
-        break; 
-      case 'X':
-        morseTweet[i][10] = "-..-";
-        break; 
-      case 'Y':
-        morseTweet[i][10] = "-.--";
-        break; 
-      case 'Z':
-        morseTweet[i][10] = "..--";
-        break; 
-      case '0':
-        morseTweet[i][10] = "----";
-        break; 
-      case '1':
-        morseTweet[i][10] = ".----";
-        break;
-      case '2':
-        morseTweet[i][10] = "..---";
-        break; 
-      case '3':
-        morseTweet[i][10] = "...--";
-        break;
-      case '4':
-        morseTweet[i][10] = "....-";
-        break;
-      case '5':
-        morseTweet[i][10] = ".....";
-        break; 
-      case '6':
-        morseTweet[i][10] = "-....";
-        break; 
-      case '7':
-        morseTweet[i][10] = "--...";
-        break; 
-      case '8':
-        morseTweet[i][10] = "---..";
-        break; 
-      case '9':
-        morseTweet[i][10] = "----.";
-        break; 
-      case '.':
-        morseTweet[i][10] = ".-.-.-";
-        break;
-      case ',':
-        morseTweet[i][10] = "--..--";
-        break;
-      case '?':
-        morseTweet[i][10] = "..--..";
-        break;
-      case '!':
-        morseTweet[i][10] = "-.-.--";
-        break;
-      case '@':
-        morseTweet[i][10] = ".--.-.";
-        break;
-      case '$':
-        morseTweet[i][10] = "...-..-";
-        break;
-      case '&':
-        morseTweet[i][10] = ".-...";
-        break;
-      case '+':
-        morseTweet[i][10] = ".-.-.";
-        break;
-      case '-':
-        morseTweet[i][10] = "-....-";
-        break;
-      case '_':
-        morseTweet[i][10] = "..--.-";
-        break;
-      case '=':
-        morseTweet[i][10] = "-...-";
-        break;
-      case ':':
-        morseTweet[i][10] = "---...";
-        break;
-      case ';':
-        morseTweet[i][10] = "-.-.-.";
-        break;
-      case '\'':
-        morseTweet[i][10] = ".----.";
-        break;
-      case '\"':
-        morseTweet[i][10] = ".-..-.";
-        break;
-      case '/':
-        morseTweet[i][10] = "-..-.";
-        break;
-      case '\\':
-        morseTweet[i][10] = "-..-.";
-        break;
-      case '(':
-        morseTweet[i][10] = "-.--.";
-        break;
-      case ')':
-        morseTweet[i][10] = "-.--.-";
-        break;
-      default: 
-        // if nothing else matches, do the default
-        // default is optional
-        break;
-    }
-  }
-}
-/*
-A = .-
-B = -...
-C = -.-.
-D = -..
-E = .
-F = ..-.
-G = --.
-H = ....
-I = ..
-J = .---
-K = -.-
-L = .-..
-M = --
-N = -.
-O = ---
-P = .--.
-Q = --.-
-R = .-.
-S = ...
-T = -
-U = ..-
-V = ...-
-W = .--
-X = -..-
-Y = -.--
-Z = ..--
-0 = ----
-1 = .----
-2 = ..---
-3 = ...--
-4 = ....-
-5 = .....
-6 = -....
-7 = --...
-8 = ---..
-9 = ----.
-. = .-.-.-
-, = --..--
-? = ..--..
-! = -.-.--
-@ = .--.-.
-$ = ...-..-
-& =  .-...
-+ = .-.-.
-- = -....-
-_ = ..--.-
-//equals = = -...-
-//colon : = ---...
-//semicolon ; = -.-.-.
-//appostophe ' = .----.
-//quote " = .-..-.
-//slash / = -..-.
-//open parans ( = -.--.
-//closed parans ) = -.--.-
-*/
-
